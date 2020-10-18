6d28dd4f30eba3eff98fff3893890a19fff03590
Kir Tankovskiy <tankovskiyko@gmail.com>
2020-10-18 16:40:11 +0300

    Initial commit

---
+++ src/main/java/Employee.java
@@ -0,0 +1,12 @@
+import lombok.Data;
+
+/**
+ * @author Kir
+ * Created on 17.10.2020
+ */
+@Data
+public class Employee {
+    private String firstName;
+    private String lastName;
+    private Integer age;
+}
